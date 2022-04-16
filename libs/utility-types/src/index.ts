cad1dcefb17caef1a8b4dce0bd3edb6019820f54
gaetanredin <gr.recowd@gmail.com>
2022-04-16 21:30:41 +0200

    feature(libs/utility-types): add coerce string decorator
    Allow coercing undefined and null to empty string
    
    Closes 6

--- libs/utility-types/src/index.ts
+++ libs/utility-types/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/boolean-input';
 export * from './lib/coerce-boolean-input.decorator';
+export * from './lib/coerce-string-input.decorator';
 export * from './lib/common';
 export * from './lib/typed-control-value-accessor';
