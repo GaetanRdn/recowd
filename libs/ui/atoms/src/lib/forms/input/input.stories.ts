5830b46758075851834a3cdb698d8eb1d931bf6f
gaetanredin <gr.recowd@gmail.com>
2022-04-16 14:47:17 +0200

    feature(libs/ui-atoms-forms): add story for template driven forms
    Closes 6

--- libs/ui/atoms/src/lib/forms/input/input.stories.ts
+++ libs/ui/atoms/src/lib/forms/input/input.stories.ts
@@ -1,11 +1,12 @@
 import { Args, Meta, moduleMetadata, Story } from '@storybook/angular';
 import { InputDirective, InputModule } from './input.directive';
 import { action } from '@storybook/addon-actions';
+import { FormsModule } from '@angular/forms';
 
 export default {
   title: 'atoms/forms/input',
   component: InputDirective,
-  decorators: [moduleMetadata({ imports: [InputModule] })],
+  decorators: [moduleMetadata({ imports: [InputModule, FormsModule] })],
   argTypes: {
     outlined: { control: false },
     valueChange: { action: 'valueChanged' },
@@ -33,3 +34,15 @@ Outlined.args = {
   ...Default.args,
   outlined: true,
 };
+
+const templateForTemplateDriven: Story = () => ({
+  props: {
+    valueChange: action('log'),
+    inputValue: '',
+  },
+  template: `
+  <p style="color: white; font-size: 16px;">inputValue: {{ inputValue }}</p>
+  <input rcInput placeholder="Type something" [(ngModel)]="inputValue" (valueChange)="valueChange($event)"/>`,
+});
+
+export const TemplateDriven = templateForTemplateDriven.bind({});
