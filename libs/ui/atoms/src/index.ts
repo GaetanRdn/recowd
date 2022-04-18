b9b16551930845c502a6017b2c87f7eaef55be93
GRdn <44368839+GaetanRdn@users.noreply.github.com>
2022-04-18 15:21:06 +0200

    19 atoms toolbar (#24)
    * feature(libs/ui-atoms): create toolbar component
    
    Closes #19
    
    * style(apps/design-system): add toolbar typography
    
    Closes #19
    
    * chore(libs/ui-atoms): patch version
    
    * 0.1.2
    
    * ci(deploy): fix deploy branch
    
    Co-authored-by: gaetanredin <gr.recowd@gmail.com>

--- libs/ui/atoms/src/index.ts
+++ libs/ui/atoms/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/button/button.directive';
 export * from './lib/forms/input/input.directive';
 export * from './lib/material-icon/material-icon.component';
+export * from './lib/toolbar/toolbar.component';
