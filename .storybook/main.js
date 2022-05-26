e8969ce74df2d70671847fd38eb59b5ab3b63084
gaetanredin <gr.recowd@gmail.com>
2022-05-26 18:49:42 +0200

    chore(all): migrate to storybook 6.5

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,12 +5,14 @@ module.exports = {
     '@geometricpanda/storybook-addon-badges',
     '@storybook/addon-jest',
     '@storybook/addon-a11y',
-  ],
-  // uncomment the property below if you want to apply some webpack config globally
+  ], // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
-
   //   // Return the altered config
   //   return config;
   // },
+  core: {
+    builder: 'webpack5',
+  },
+  staticDirs: ['../src/assets'],
 };
