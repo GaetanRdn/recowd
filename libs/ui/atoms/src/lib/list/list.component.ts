12974729e4a61dae66e44d5c93ffcff394d139c0
gaetanredin <gr.recowd@gmail.com>
2022-07-17 23:10:09 +0200

    feature: create list atom
    Closes #60

--- libs/ui/atoms/src/lib/list/list.component.ts
+++ libs/ui/atoms/src/lib/list/list.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { BooleanInput, CoerceBoolean } from '@recowd/utility-types';
 
 @Component({
   selector: 'rc-list',
@@ -7,10 +8,14 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
     '[class.rc-horizontal]': 'horizontal',
   },
   template: ` <ng-content select="[rcListTitle], rc-list-title"></ng-content>
-    <ng-content select="[rcListItem], rc-list-item"></ng-content>`,
+    <span class="rc-list-items-wrapper">
+      <ng-content select="[rcListItem], rc-list-item"></ng-content>
+    </span>`,
   styleUrls: ['./list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ListComponent {
-  @Input() public horizontal = false;
+  static ngAcceptInputType_horizontal: BooleanInput;
+
+  @Input() @CoerceBoolean() public horizontal = false;
 }
